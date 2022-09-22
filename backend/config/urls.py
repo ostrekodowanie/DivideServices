75d1c6ae075b462335ac45258691214f394f2ebe
Sebastian <se6359@gmail.com>
2022-09-22 23:18:04 +0200

    urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('services', views.index),
+    path('contact', views.index),
     path('', include('apps.Product.urls')),
 ]
