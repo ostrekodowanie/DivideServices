30e832065513ec0cb75b92de00a10f9383dcf7ce
Sebastian <se6359@gmail.com>
2022-10-12 01:08:58 +0200

    settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -155,7 +155,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 AUTH_USER_MODEL = 'Auth.User'
