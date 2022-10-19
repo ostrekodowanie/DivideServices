3c89952c4f72af66d29f89115c449e3bb05a30a3
Sebastian <se6359@gmail.com>
2022-10-19 23:04:09 +0200

    s

--- backend/apps/Product/urls.py
+++ backend/apps/Product/urls.py
@@ -6,5 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('api/products', views.ProductView.as_view()),
     path('api/products/<slug>', views.ProductRetrieveView.as_view()),
-    path('api/products/<int:pk>', views.ProductDetailView.as_view()),
+    path('api/products/details/<pk>', views.ProductDetailView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
