7af4d861053d6631791942cb234b660643d2ebfd
Sebastian <se6359@gmail.com>
2022-10-21 20:03:31 +0200

    media

--- backend/apps/Product/urls.py
+++ backend/apps/Product/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('products/<slug>', views.products),
     path('api/products', views.ProductView.as_view()),
     path('api/products/<slug>', views.ProductRetrieveView.as_view()),
     path('api/products/details/<pk>', views.ProductDetailView.as_view()),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path('images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
