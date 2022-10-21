abf94815bdad5cc7b183b943bd38606d1f05ece5
Sebastian <se6359@gmail.com>
2022-10-21 19:39:15 +0200

    .

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -140,13 +140,15 @@ USE_TZ = True
 STATIC_URL = 'assets/'
 
 if DEBUG:
-        STATICFILES_DIRS = [os.path.join(BASE_DIR, '../frontend/dist/assets')]
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, '../frontend/dist/assets'),
+        ]
 else:
         STATIC_ROOT = os.path.join(BASE_DIR, '../frontend/dist/assets')
 
-MEDIA_ROOT = BASE_DIR / 'images'
-
 MEDIA_URL = 'images/'
+
+MEDIA_ROOT = BASE_DIR / 'images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
